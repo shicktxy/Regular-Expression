2aefa44576e20c5166ad950dc9075249cf61528a
shicktxy <56121106+shicktxy@users.noreply.github.com>
2019-10-05 11:02:44 +0800

    Update regular-expression.c

--- regular-expression.c
+++ regular-expression.c
@@ -4,8 +4,8 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
-int RegexCheck(const char *original_regex_str){  //check if the first char of regex is '*' or if the expression has part like "**"
-	if (*original_regex_str == '*')
+int RegexCheck(const char *original_regex_str){  //Check the regex santax ,return error if the regex is started with * or
+	if (*original_regex_str == '*')              //has part like ** 
 		return -1;
 	for (int i = 0; i < strlen(original_regex_str); i++) {
 		if (original_regex_str[i] == '*'&&original_regex_str[i - 1] == '*')
@@ -14,7 +14,7 @@ int RegexCheck(const char *original_regex_str){  //check if the first char of re
 	return 0;
 }
 
-char* RegexStringRevise(const char *original_regex_str) {  //delete the iterative part such as "a*a" and "a*a*" in the regular expression and the error part like "a**"
+char* RegexStringRevise(const char *original_regex_str) {  //Delete the repeated part like a*a or a*a* in the regex
 	int regex_str_len = strlen(original_regex_str);  
 	char *temp = (char*)malloc((regex_str_len + 1) * sizeof(char));
 	int i=0,j=0;
@@ -37,98 +37,89 @@ char* RegexStringRevise(const char *original_regex_str) {  //delete the iterativ
 	return temp;
 }
 
-bool RegexCompare(const char *original_str, const char *original_regex_str) {
-	if (RegexCheck(original_regex_str))
-		return false;  //input regex santax error
-	char *regex_str;
-	regex_str = RegexStringRevise(original_regex_str);
-
-	char *point_regex = regex_str;
-	char *point_original = original_str;
-
-	if (*point_regex == '\0' && *point_original == '\0') {  //deal with expression with empty string
-		free(regex_str);
-		return true;
-	}
-	else if (*point_regex == '\0' && *point_original != '\0') {
-		free(regex_str);
-		return false;
-	}
-	else if (*point_regex != '\0' && *point_original == '\0') {
-		while (*point_regex != '\0') {
-			if (*(point_regex + 1) == '*')
-				point_regex += 2;
-			else {
-				free(regex_str);
-				return false;
-			}
+int GetAllowed(char *regex_str, char *allowed) { //get possible char to compare of every bit of the regex
+	int k = 0;                                   
+	char *temp = regex_str;
+	if (*(temp + 1) != '*')
+		allowed[k] = *temp;
+	else if (*(temp + 1) == '*') {
+		while (*temp != '\0' && *(temp + 1) == '*') {
+			allowed[k] = *temp;
+			k++;
+			temp += 2;
 		}
-		free(regex_str);
-		return true;
+		allowed[k] = *temp;
 	}
+	return k+1;
+}
 
-	while (*point_regex != '\0' && *point_original != '\0') {
-		if ((*point_regex <= 'z' && *point_regex >= 'a') || *point_regex == '.') {
-			if (*(point_regex + 1) != '*') {
-				if (*point_regex != '.') {
-					if (*point_original == *point_regex) {
-						point_original++;
-						point_regex++;
-					}
-					else {
-						break;  //compare failed
-					}
-				}
-				else if (*point_regex == '.') {
-					point_original++;
-					point_regex++;
-				}
-			}
-			if (*(point_regex + 1) == '*') {
-				if (*point_regex != '.'&&*(point_regex + 2) != '.') {
-					if (*point_original == *(point_regex + 2)) {
-						point_regex += 2;
-					}
-					else if (*point_original == *point_regex) {
-						point_original++;
-						if (*point_original == '\0')
-							point_regex += 2;
-					}
-					else
-						break;
-				}
-				if (*point_regex != '.'&&*(point_regex + 2) == '.')
-				{
-					if (*point_original == *point_regex) {
-						point_original++;
-						if (*point_original == '\0')
-							point_regex += 2;
-					}
-					else {
-						point_regex += 2;
-					}
-				}
-				if (*point_regex == '.'&& *(point_regex + 2) != '.') {
-					if (*point_original == *(point_regex + 2)) {
-						point_regex += 2;
-					}
-					else {
-						point_original++;
-						if (*point_original == '\0')
-							point_regex += 2;
-					}
-				}
-			}
+bool BitCompare(char **original_str,char **regex_str, char *allowed, int n) { //compare every bit of the original string with the regex
+	int k;																	  //use pointer to pointer to change the value of pointer
+	if (n == 1) {
+		if (**original_str == allowed[0]) {
+			(*original_str)++;
+			(*regex_str)++;
+			return true;
 		}
-		else
-			break;
-	}
-
-	if (*point_original == '\0'&&*point_regex == '\0') {
-		free(regex_str);
-		return true;
+		else if (allowed[0] == '.') {
+			(*original_str)++;
+			(*regex_str)++;
+			return true;
+		}
 	}
 	else {
+		for (k = 0; k < n; k++) {  //firstly check a to z
+			if (allowed[k] != '.' && **original_str == allowed[k]) {
+				if (k != n - 1) {
+					(*original_str)++;
+					(*regex_str) += k * 2;
+					return true;
+				}
+				else {
+					(*regex_str) += k * 2;  //exit the * algorithm
+					return true;
+				}
+			}
+		}
+		for (k = 0; k < n; k++) {  //check .
+			if (allowed[k] == '.') {
+				if (k != n - 1) {
+					(*original_str)++;
+					(*regex_str) += k * 2;
+					return true;
+				}
+				else {
+					(*regex_str) += k * 2;  //exit the * algorithm
+					return true;
+				}
+			}
+			
+		}
+	}
+	return false;  //fail to compare ,return 0
+}
+
+bool RegexCompare(const char *original_str, const char *original_regex_str) {  //compare the original string with the regex
+	if (RegexCheck(original_regex_str))
+		return false;  //regex santax error
+	char *regex_str;
+	regex_str = RegexStringRevise(original_regex_str);  //revise the regex
+	char allowed[50]; 
+	int count;
+	char *point_regex = regex_str;  //generate the pointer to every bit of the string
+	char *point_original = original_str;
+
+	while (*point_regex != '\0' || *point_original != '\0') {  //compare bit by bit 
+		count = GetAllowed(point_regex, allowed);
+		if (BitCompare(&point_original, &point_regex, allowed, count) == false) {
+			break;
+		}
+	}
+	if (*point_original == '\0'&&*point_regex == '\0') {  //success
+		free(regex_str);
+		return true;
+	}
+	else {  //fail
 		free(regex_str);
 		return false;
 	}
